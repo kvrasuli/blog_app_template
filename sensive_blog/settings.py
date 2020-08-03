22993d5b144a146f633b2027f17f9cf3b476701b
Kirill Rasuli <rasulikv@gmail.com>
2020-08-03 22:00:17 +0300

    Fix the templates, fix the static path

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -96,12 +96,7 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/img/banner'),
-                    os.path.join(BASE_DIR, 'static/img/blog'),
-                    os.path.join(BASE_DIR, 'static/img/blog/blog-slider'),
-                    os.path.join(BASE_DIR, 'static/fonts'),
-                    os.path.join(BASE_DIR, 'static/'),
-                    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
